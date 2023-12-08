14e2c45d6b12f8befa8edbb1776c94d0aa1efa67
Gaston Chenet <81047473+du-cassoulet@users.noreply.github.com>
2023-12-08 10:31:58 +0100

    Update getUsers.tsx

--- src/utils/getUsers.tsx
+++ src/utils/getUsers.tsx
@@ -1,7 +1,7 @@
 import type { UserProfile } from "@/types/UserProfile";
 
 export default async function getUsers(): Promise<UserProfile[]> {
-	const users = await fetch("http://129.151.234.121:80/api/users", {
+	const users = await fetch("http://129.151.234.121:8080/api/users", {
 		next: {
 			revalidate: 3600,
 		},
