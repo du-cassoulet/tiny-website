bfa664207403fc4fa79c28e2308c6849b2f3b7e6
Gaston Chenet <81047473+du-cassoulet@users.noreply.github.com>
2023-12-08 10:32:54 +0100

    Update getCommands.tsx

--- src/utils/getCommands.tsx
+++ src/utils/getCommands.tsx
@@ -1,7 +1,7 @@
 import type { Command } from "@/types/Command";
 
 export default async function getCommands(): Promise<Command[]> {
-	const commands = await fetch("http://129.151.234.121:80/api/commands", {
+	const commands = await fetch("http://129.151.234.121:8080/api/commands", {
 		next: {
 			revalidate: 86_400,
 		},
