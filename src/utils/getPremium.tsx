048f767d3b4aca348f9d18136e121b697e55e8e6
Gaston Chenet <81047473+du-cassoulet@users.noreply.github.com>
2023-12-08 10:32:40 +0100

    Update getPremium.tsx

--- src/utils/getPremium.tsx
+++ src/utils/getPremium.tsx
@@ -1,7 +1,7 @@
 import type { PremiumSlot } from "@/types/PremiumSlot";
 
 export default async function getPremium(): Promise<PremiumSlot[]> {
-	const res = await fetch("http://129.151.234.121:80/api/premium", {
+	const res = await fetch("http://129.151.234.121:8080/api/premium", {
 		next: {
 			revalidate: 86_400,
 		},
